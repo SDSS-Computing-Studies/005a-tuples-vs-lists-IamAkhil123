320a58fa8b23ce2fc2fe8063f7041d05faab6cb4
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-14 10:16:25 -0800

    updatae

--- task2.py
+++ task2.py
@@ -1,5 +1,15 @@
 #!python3
 
+x = input("Enter a word:") 
+y = input("Enter a word:") 
+z = input("Enter a word:") 
+a = input("Enter a word:") 
+b = input("Enter a word:") 
+
+List = [x, y, z, a, b]
+
+print(List)
+
 """
 Create a variable that contains an empy list.
 Ask a user to enter 5 words.  Add the words into the list.
