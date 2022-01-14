76e6a105acc1c19ad6d1b6c5f4a8f998db1b33e1
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-14 13:51:32 -0800

    update

--- task3.py
+++ task3.py
@@ -1,4 +1,9 @@
 #! python3
+
+myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+mynewlist = sorted(myList)
+print("The smallest number is", mynewlist[0] ,"and the largest number is", mynewlist[11])
+
 """
 Sort the given list by numerical value
 Find the smallest and the largest value and display them:
@@ -12,5 +17,3 @@ string containing the 2 numbers:
 example:
 The smallest number is 3 and the largest number is 9
 """
-
-myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
